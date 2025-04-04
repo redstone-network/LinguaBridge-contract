d1308a6c3f8a98423392f9d4ef047970a4663b65
Li Smith <jinrishouji@sina.com>
2025-04-04 15:07:25 +0800

    add deploy:zerog

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    zerog: {
+      url: "https://evmrpc-testnet.0g.ai",
+      accounts: [process.env.ZEROG_PRIVATE_KEY]
+    }
+  }
 };
