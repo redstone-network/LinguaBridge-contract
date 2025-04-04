ef02df41748abaa5055d7d972b4e8a72fe799305
Li Smith <jinrishouji@sina.com>
2025-04-04 15:20:05 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,17 +5,19 @@ async function main() {
   const initialSupply = 1000000; // 初始供应量
   const KnowledgeToken = await hre.ethers.getContractFactory("KnowledgeToken");
   const knowledgeToken = await KnowledgeToken.deploy(initialSupply);
-  await knowledgeToken.deployed();
-  console.log("知识库代币部署至:", knowledgeToken.address);
+  await knowledgeToken.waitForDeployment();
+  const tokenAddress = await knowledgeToken.getAddress();
+  console.log("知识库代币部署至:", tokenAddress);
 
   // 部署知识库文件夹合约
   const IndustryKnowledgeFolder = await hre.ethers.getContractFactory("IndustryKnowledgeFolder");
-  const knowledgeFolder = await IndustryKnowledgeFolder.deploy(knowledgeToken.address);
-  await knowledgeFolder.deployed();
-  console.log("知识库文件夹部署至:", knowledgeFolder.address);
+  const knowledgeFolder = await IndustryKnowledgeFolder.deploy(tokenAddress);
+  await knowledgeFolder.waitForDeployment();
+  const folderAddress = await knowledgeFolder.getAddress();
+  console.log("知识库文件夹部署至:", folderAddress);
 
   // 将知识库文件夹合约设置为代币铸造者
-  const addMinterTx = await knowledgeToken.addMinter(knowledgeFolder.address);
+  const addMinterTx = await knowledgeToken.addMinter(folderAddress);
   await addMinterTx.wait();
   console.log("已将知识库文件夹设置为代币铸造者");
 }
@@ -25,4 +27,4 @@ main()
   .catch((error) => {
     console.error(error);
     process.exit(1);
-  }); 
+  });
